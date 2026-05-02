199ae49b897e3cfa92e250836f10c560f765f81a
indecisivenitin <ahir6346@gmail.com>
2026-05-02 19:40:31 +0530

    changes fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,9 @@
-// import ExploreBtn from "@/components/ExploreBtn";
-// import EventCard from "@/components/EventCard";
-// import {IEvent} from "@/database";
-// import {cacheLife} from "next/cache";
-
-// const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-// const Page = async () => {
-//     'use cache';
-//     cacheLife('hours')
-//     const response = await fetch(`${BASE_URL}/api/events`);
-//     const { events } = await response.json();
-
-//     return (
-//         <section>
-//             <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
-//             <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
-
-//             <ExploreBtn />
-
-//             <div className="mt-20 space-y-7">
-//                 <h3>Featured Events</h3>
-
-//                 <ul className="events">
-//                     {events && events.length > 0 && events.map((event: IEvent) => (
-//                         <li key={event.title} className="list-none">
-//                             <EventCard {...event} />
-//                         </li>
-//                     ))}
-//                 </ul>
-//             </div>
-//         </section>
-//     )
-// }
-
-// export default Page;
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
 
-const BASE_URL = "http://localhost:3000"; // ✅ key fix
+
 
 const Page = async () => {
     'use cache';
@@ -48,7 +12,7 @@ const Page = async () => {
     let events: IEvent[] = [];
 
     try {
-        const response = await fetch(`${BASE_URL}/api/events`);
+        const response = await fetch("https://dev-event-next-js-16.vercel.app/api/events");
 
         if (!response.ok) throw new Error("Failed");
 
